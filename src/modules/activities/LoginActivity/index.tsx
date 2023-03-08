9607d35edfd5620aa86561ccfd7267f105aa710f
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2023-03-08 08:27:59 +0200

    ft(Develop login page):
    -Add logo
    -Add wecome title
    -Add login button
    
    [WIP]

--- src/modules/activities/LoginActivity/index.tsx
+++ src/modules/activities/LoginActivity/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from "./LoginActivity.module.css";
+import logo from "../../../img/logo.png";
 
 const LoginActivity = () => {
   return (
@@ -9,9 +10,21 @@ const LoginActivity = () => {
           <h2>You will never regret to join Codehills</h2>
           <p>We're glad you're here! Please log in to explore all of our features</p>
         </div>
-      
+
       </div>
-      <div className={classes.loginRight}>Right</div>
+      <div className={classes.loginRight}>
+        <div className={classes.righDdata}>
+          <div className={classes.imgContainer}>
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className={classes.loginwelcome}>
+            <h2>Welcome</h2>
+          </div>
+          <div>
+            <button>Loggin With microseoft</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };

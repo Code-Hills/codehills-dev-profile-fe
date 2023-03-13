238c92c2a9ac22cd83258fdba90e1dfdf1d716b6
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2023-03-13 09:52:28 +0200

    Fix yarn conflict

--- src/modules/activities/LoginActivity/index.tsx
+++ src/modules/activities/LoginActivity/index.tsx
@@ -1,43 +1,18 @@
 import React from 'react';
+import classes from "./LoginActivity.module.css";
 
 const LoginActivity = () => {
   return (
-    <div className="m-auto w-full max-w-md min-h-[350px] mt-6 md:mt-8 flex flex-col justify-center rounded-[30px] bg-slate-300/70 shadow-sm p-4 md:p-12">
-      <h1 className="text-center text-2xl md:text-4xl text-blue-800">
-        Login Codehills
-      </h1>
-      <form className="mt-6 md:mt-8 flex flex-col w-full">
-        <label htmlFor="email" className="flex flex-col space-y-1">
-          <span className="text-gray-600">Email</span>
-          <input
-            id="email"
-            type="text"
-            placeholder="Email"
-            className="p-2 rounded border-gray-400 bg-white w-full"
-          />
-        </label>
-
-        <label
-          htmlFor="password"
-          className="flex flex-col space-y-1 mt-2"
-        >
-          <span>Password</span>
-          <input
-            id="password"
-            type="password"
-            placeholder="Password"
-            className="p-2 rounded border-gray-400 bg-white w-full"
-          />
-        </label>
-
-        <button
-          type="submit"
-          className="rounded mt-6 md:mt-8 py-3 px-3 bg-blue-800 text-white font-semibold px-8 md:px-12"
-        >
-          Login
-        </button>
-      </form>
-    </div>
+    <section className={classes.login}>
+      <div className={classes.loginLeft}>
+        <div className={classes.loginHeader}>
+          <h2>You will never regret to join Codehills</h2>
+          <p>We're glad you're here! Please log in to explore all of our features</p>
+        </div>
+      
+      </div>
+      <div className={classes.loginRight}>Right</div>
+    </section>
   );
 };
 

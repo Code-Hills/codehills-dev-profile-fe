d89a7f7d2b950068cae0448d87e1097bd839215d
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-04-13 17:13:53 +0200

    add app favicon

--- src/__tests__/activities/Dashboard.test.tsx
+++ src/__tests__/activities/Dashboard.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import DashboardActivity from '@/modules/activities/DashboardActivity';
 import Secure from '@/utils/secureLs';
+import store from '@/redux/store';
 
 test('renders DashboardActivity correctly', async () => {
   const { findAllByText } = render(
-    <Router>
-      <DashboardActivity />
-    </Router>,
+    <Provider store={store}>
+      <Router>
+        <DashboardActivity />
+      </Router>
+    </Provider>,
   );
   const elements = await findAllByText(/Codehills/i);
   expect(elements.length).not.toBe(0);

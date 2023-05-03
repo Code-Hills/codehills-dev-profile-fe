ba744d9fd24db1e960d989d4a48f728ff6d54bfc
bizip <bizip04@gmail.com>
2023-05-03 21:57:19 +0200

    center pagination

--- src/modules/activities/AdminDashboard/UsersActivity/index.tsx
+++ src/modules/activities/AdminDashboard/UsersActivity/index.tsx
@@ -141,8 +141,8 @@ const UsersActivity = () => {
                 </tbody>
               </table>
             </div>
-          <nav aria-label="Page navigation example">
-            <ul class="inline-flex items-center -space-x-px">
+          <nav aria-label="Page navigation example ">
+            <ul class="flex items-center justify-center -space-x-px w-full mb-10 mt-8">
               <li>
                 <a href="#" class="block flex px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                   <span>Previous</span>

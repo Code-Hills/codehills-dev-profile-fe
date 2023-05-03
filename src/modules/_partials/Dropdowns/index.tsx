6999d162ce9052c26ea3f6106e8a82399e157759
bizip <bizip04@gmail.com>
2023-05-03 21:57:19 +0200

    center pagination

--- src/modules/_partials/Dropdowns/index.tsx
+++ src/modules/_partials/Dropdowns/index.tsx
@@ -40,7 +40,7 @@ const DropdownMenu = () => {
         } bg-white divide-gray-100 rounded-lg shadow dark:bg-gray-700`}
       >
         <ul
-          className="py-2 bg-gray-200 text-sm text-gray-700 dark:text-gray-700 w-40 absolute"
+          className="py-2 z-50 bg-gray-200 text-sm text-gray-700 dark:text-gray-700 w-40 absolute"
           aria-labelledby="dropdownDelayButton"
         >
           <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">

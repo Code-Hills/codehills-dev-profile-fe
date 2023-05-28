2551603bac8f7d09c4f1aee95b75a796670431cb
bizip <bizip04@gmail.com>
2023-05-28 08:45:37 +0200

    Fix conflict from user.tsx file

--- src/modules/_partials/Dropdowns/index.tsx
+++ src/modules/_partials/Dropdowns/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 
 const DropdownMenu = () => {
@@ -41,7 +40,7 @@ const DropdownMenu = () => {
         } bg-white divide-gray-100 rounded-lg shadow dark:bg-gray-700`}
       >
         <ul
-          className="py-2 z-50 bg-gray-200 text-sm text-gray-700 dark:text-gray-700 w-40 absolute"
+          className="py-2 bg-gray-200 text-sm text-gray-700 dark:text-gray-700 w-40 absolute"
           aria-labelledby="dropdownDelayButton"
         >
           <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">

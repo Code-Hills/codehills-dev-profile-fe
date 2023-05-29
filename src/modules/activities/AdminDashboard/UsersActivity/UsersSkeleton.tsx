ef7a97eb7c3ffd72d08ead367bf8bbec12f40681
bizip <bizip04@gmail.com>
2023-05-29 09:43:02 +0200

    remove duplicates ;of the skeleton elements

--- src/modules/activities/AdminDashboard/UsersActivity/UsersSkeleton.tsx
+++ src/modules/activities/AdminDashboard/UsersActivity/UsersSkeleton.tsx
@@ -1,11 +1,12 @@
 import SkeletonElement from '@/modules/_partials/shared/SkeletonElement';
 
 const UsersSkeleton = () => {
+  const skeletons = [1, 2, 3];
   return (
     <div className="flex justify-between flex-col  bg-red-500 dark:bg-gray-800  p-2">
-      <SkeletonElement className="w-full h-16 rounded" />
-      <SkeletonElement className="w-full h-16 rounded " />
-      <SkeletonElement className="w-full h-16 rounded" />
+      {skeletons.map(item => (
+        <SkeletonElement key={item} className="w-full h-16 rounded" />
+      ))}
     </div>
   );
 };
